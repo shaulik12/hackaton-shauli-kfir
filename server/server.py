2ffede9438155150808ba6b50c0e7e5b3046778a
shaulik12 <shaulik@post.bgu.ac.il>
2021-12-28 15:01:04 +0200

    done threading communications hopefully for now

--- server/server.py
+++ server/server.py
@@ -5,7 +5,8 @@ import threading
 import struct
 
 HOSTIP = socket.gethostbyname(socket.gethostname())     #finds name of host running thethe program, then translates it to IP
-UDPPORT = 13117             #predetermined udp listening port for clients                  
+UDPPORT = 13117             #predetermined udp listening port for clients
+TCPPORT = 2086              #our team tcp port
 GAMETIMEOUT = 10            #game wait timeout
 MAXCLIENTS = 2              #maximum number of allowed clients
 UDPFREQUENCY = 1            #delay between each UPD broadcast message
@@ -13,18 +14,26 @@ MAGICCOOKIE = 0xabcddcba    #UDP message confirmation cookie
 OFFER = 0x2                 #UDP "offer" message flag
 
 class Client:
+    clientCountLock = threading.Lock()
+    connected = 0
     def __init__(self, socket, addr):
         self.socket = socket
         self.addr = addr
         self.teamName = ""
     def setTeamName(self, teamName):
         self.teamName = teamName
+        with Client.clientCountLock:
+            Client.connected += 1
+            if Client.connected >= MAXCLIENTS:
+                gameStart.set()
     def disconnect(ansLock, client):
-        clientsReady.set()     #tells game to stop waiting
         if not riddleAnswered.is_set():
             ansLock.giveAnswer(-1, client.teamName) #tell game the player no longer playes (gives impossible answer)
         connectedClients.remove(client)     #remove client from connected clients list
-        gameNotPlayed.set()   #game no longer on max clients after 1one disconnects
+        with Client.clientCountLock:
+            Client.connected -= 1
+        gameOver.set()   #game no longer on max clients after 1one disconnects
+        
         
      
 class GameMsgLock:        
@@ -41,7 +50,6 @@ class GameMsgLock:
             self.clientsUsedMsg += 1
             if (self.clientsUsedMsg >= MAXCLIENTS):
                 gameMsgUpdated.clear()
-                clientsReady.set()
 
 class AnswerLock:
     def __init__(self):
@@ -61,24 +69,19 @@ class AnswerLock:
         finally:
             return solution
 
-tcpPort = -1                #TCP port used for communication
 connectedClients = list()   #list of connected clients
 threads = list()            #list of active threads
-tcpPortInitialized = threading.Event()  #tells UPD transmitted that the TCP port is initialized
 gameMsgUpdated = threading.Event()      #tells clients theres a new game message
-#gameNotPlayed = threading.Event()           #tells clients the game is over
 clientsReady = threading.Event()        #all clients notify the game
 riddleAnswered = threading.Event()      #tells the game a client has a solution
 gameStart = threading.Event()           #tells the game it should start
+gameOver = threading.Event()  
 underMaxClients = threading.Event()     #server has less than the maxium number of players connected
 underMaxClients.set()
-#gameNotPlayed.set()
 
-# add extention "remove ssh"
+#add extention "remove ssh"
 #choose the hackaton from the configuration file on the search bar on top
 #then open file
-
-#on client: tcpSocket.setsockopt(socket.SOL_SOCKET, socket.TCP_NODELAY, 1)
 
 def Main():
     ansLock = AnswerLock()
@@ -94,14 +97,11 @@ def Main():
     broadcastThread.join()
 
 def tcpInit(ansLock, gameMsgLock):
-    tcpSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+    tcpSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     with tcpSocket:
-        tcpSocket.bind((HOSTIP,0))
-        global tcpPort
-        tcpPort = tcpSocket.getsockname()[1]
+        tcpSocket.bind((HOSTIP, TCPPORT))
         tcpSocket.listen(MAXCLIENTS)
         print("Server started, listening on IP address ", HOSTIP)
-        tcpPortInitialized.set()
         while True:
             while len(connectedClients) < MAXCLIENTS:
                 conn, addr = tcpSocket.accept()
@@ -110,15 +110,12 @@ def tcpInit(ansLock, gameMsgLock):
                 connectedClients.append(newClient)               
                 threads.append(newThread)
                 newThread.start()
-            maxClients.set()            #notify max number of clients is reached
-            underMaxClients.clear()
-            gameNotPlayed.wait()      #wait until the game has ended
-            gameNotPlayed.clear()
+            underMaxClients.clear()     #stops udp
+            gameStart.set()
+            gameOver.wait()
             threads = [t for t in threads if t.is_alive()]  #clean dead threads
             if len(threads) < MAXCLIENTS:
-                maxClients.clear()
                 underMaxClients.set()
-            print("Game over, sending out offer requests...")
             
 def tcpTalk(client, ansLock, gameMsgLock):
     with client.socket as socket:
@@ -126,13 +123,16 @@ def tcpTalk(client, ansLock, gameMsgLock):
         if teamName is not None:                #if connection hasn't ended
             client.setTeamName(teamName)        #sets the new team name
             gameMsgUpdated.wait()                #wait for game to give math question
-            socket.sendall(gameMsgLock.gameMsg) #sends math question to client
-            gameMsgLock.msgUsed(socket)         #inform game message was sent
-            answer = readClientAnswer(socket)   #read user answer (only 1 character)
-            if answer is not None:                      #if connection hasn't ended
-                ansLock.giveAnswer(answer,teamName)     #send answer to game
-                gameMsgUpdated.wait()                    #waits for game to give winner message
-                socket.sendall(gameMsgLock.gameMsg)     #sends game conclusion
+            try:
+                socket.sendall(gameMsgLock.gameMsg) #sends math question to client
+                gameMsgLock.msgUsed(socket)         #inform game message was sent
+                answer = readClientAnswer(socket)   #read user answer (only 1 character)
+                if answer is not None:                      #if connection hasn't ended
+                    ansLock.giveAnswer(answer,teamName)     #send answer to game
+                    gameMsgUpdated.wait()                   #waits for game to give winner message
+                    socket.sendall(gameMsgLock.gameMsg)     #sends game conclusion
+            except:
+                socket.close()
     Client.disconnect(client)
 
 def readTeamName(clientSocket):
@@ -157,15 +157,12 @@ def readClientAnswer(clientSocket):
 def game(answerLock, gameMsgLock):
     while True:
         gameStart.wait()                   #wait until 2 clients are conected
-        #TODO wait until all connected clients set their name
-        sleep(GAMETIMEOUT)                  #after second client connects, wait 10 seconds for game to begin
+        sleep(GAMETIMEOUT)                 #after second client connects, wait 10 seconds for game to begin
         try:
             teamNames = [conn.teamName for conn in connectedClients]
             riddle, ans = mathGenerator()   #generate simple math riddle and answer
             startMsg = gameStartMessage(teamNames, riddle)
             gameMsgLock.setMsg(startMsg)    #tells clients to send the message
-            clientsReady.wait()            #waiting for game message to be sent to all clients
-            clientsReady.clear()
             isDraw = not riddleAnswered.wait(GAMETIMEOUT)  #waiting for riddle answer or 10 seconds
             riddleAnswered.clear()
             solver, guess = answerLock.checkSolution()
@@ -174,9 +171,8 @@ def game(answerLock, gameMsgLock):
                 solver = teamNames[0]
             endMsg = gameOverMessage(isDraw, solver, ans)
             gameMsgLock.setMsg(endMsg) 
-            clientsReady.wait()            #waiting for message to be sent and clients to log out. prevents game restart
+            print("Game over, sending out offer requests...")
         finally:
-            clientsReady.clear()
             gameStart.clear()                  #tells tcpInit thread the game ended nad clients are logged out
 
 def gameStartMessage(teamNames, riddle):
@@ -219,13 +215,12 @@ def udpBroadcast():
     with udpSocket:
         udpSocket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)     #at the socket level, set the broadcast option to 'on'
         #udpSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)    #run on linux
-        udpSocket.bind(("", 0))
-        tcpPortInitialized.wait()                                           #waits for TCP port to initialize
+        udpSocket.bind(("", 0))                                             #waits for TCP port to initialize
         while True:
             if len(connectedClients) >= MAXCLIENTS:
                 underMaxClients.wait()                                      #stop threat if max clients connected
             try:
-                byteMsg = struct.pack('IbH', MAGICCOOKIE, OFFER, tcpPort)   #creates message to be sent
+                byteMsg = struct.pack('IbH', MAGICCOOKIE, OFFER, TCPPORT)   #creates message to be sent
                 udpSocket.sendto(byteMsg, ('<broadcast>',UDPPORT))          #attempts to send message
             except:
                 print("exception occured during udp broadcast trasmission")
